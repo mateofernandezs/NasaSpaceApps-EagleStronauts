1e6c9a540490bf412a431084a3516a16e87a2733
emi <emi.ribera.velasquez@gmail.com>
2024-10-05 20:08:22 -0400

    solar system done, scene, planets and the sun. Very basic

--- spaceapps-orrery/src/main.jsx
+++ spaceapps-orrery/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import ThreeScene from './components/Orrey/orrey_scene';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThreeScene />
   </React.StrictMode>,
 )
